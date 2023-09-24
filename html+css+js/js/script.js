5cba6bcd6db287c6cc8c3ba5f640b216fdfe4baa
crazqx <crazqx949@gmail.com>
2023-09-24 20:50:32 +0300

    add js

---
+++ html+css+js/js/script.js
@@ -0,0 +1,20 @@
+// Комментарии в JavaScript (однострочный)
+
+/*
+многострочный
+*/
+
+// document.write("JavaScript"); выводит информацию сразу после тега script
+// console.log("JavaScript"); выводит текст в консоль разработчика
+// console.info("JavaScript");
+// console.error("JavaScript error"); Выводит текст в виде ошибки
+// console.warn("Warning JavaScript"); Выводит уведомление
+
+
+// ПЕРЕМЕННЫЕ
+var number; // var - создает переменную number
+num = 7; // значение переменной равно 7
+
+var num1 = 5; // упрощенная версия
+console.log("Переменная: " + num) /* В консоли будет выведена
+переменная и ее значение 7 (Переменная: 7) */
